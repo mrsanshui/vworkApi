e0e2ebda290d15e1a91b9c343af3d7a53227c3e4
三水君 <437382693@qq.com>
2024-03-08 20:16:33 +0800

    vworkApi V-2.0.0 1、优化接口架构、使用更简洁 2、增强社区版的稳定性 3、更新Python版的调用demo 4、修复若干细节问题...

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # vworkApi介绍
 
-
-
-《vworkApi》是基于PC端的企业微信封装的、REST风格的接口，开发者可通过**HTTP轻松调用**，真正实现“跨语言”操作！
+《vworkApi》是基于PC端的企业微信封装的、REST风格的接口，开发者可通过**HTTP轻松调用**。可进行二次开发，实现微信机器人、群管理等强大的功能！
 
 
 
@@ -11,9 +9,14 @@
 - 社区版：
   - 麻雀虽小五脏俱全！
   - 不支持长时间运行
+  - 不再维护更新
+  - 微信版本：4.0.0.6024
+  
 - 专业版：
   - 功能更加强大、稳定！
   - 支持长时间运行
+  - 使用有保障！持续更新迭代
+  - 微信版本：4.1.13.6002
 
 
 
@@ -21,27 +24,49 @@
 
 
 
-## 使用教程（仅需3步）
+## 使用教程
 
-> <span style="color: #ff5050">使用前请先安装指定版本的微信</span>
+> <span style="color: #ff5050">使用前请先安装指定版本的企业微信</span>
 >
-> 指定版本微信安装包：[https://pan.baidu.com/s/1FKlfwVsFLOhAKYlpSWSSMA](https://pan.baidu.com/s/1FKlfwVsFLOhAKYlpSWSSMA)
+> 指定版本企业微信安装包：[https://pan.baidu.com/s/1FKlfwVsFLOhAKYlpSWSSMA](https://pan.baidu.com/s/1FKlfwVsFLOhAKYlpSWSSMA)
 >
 > 提取码：sszs
 
 
 
-- 克隆该项目（<span style="color: #ff5050">请关闭你的杀毒软件，否则可能会误删dll文件</span>）
+1. 克隆该项目（<span style="color: #ff5050">请关闭你的杀毒软件，否则可能会误删dll文件</span>）
 
-1. 开启一个http服务，并且运行在 `9000` 端口上，请求路径为 `/msg`
 2. 运行《注入工具(图形界面版).exe》点击启动并注入
-3. 注入成功后，即可向 `8989` 端口发送http请求，执行对应操作，请求路径为 `/api`
+
+3. 监听消息：开启一个HTTP服务，并且运行在 `9000` 端口上，请求路径为 `/msg`，请求方法为 `POST`
+
+4. 操作接口：注入成功后，即可向 `8989` 端口发送HTTP请求，执行对应操作，请求路径为 `/api`，请求方法为 `POST`
+5. 文档地址：https://www.showdoc.com.cn/mrsanshui/9693807382610096
 
 
 
-> 如需多开并自定义端口号，命令行调用《inject_tool.exe》即可
->
-> 启动命令：[exe的全路径] start [要开启的端口号]
+## 更灵活的使用方法（使用命令行注入）
+
+如需更灵活的使用，可使用命令行调用《inject_tool.exe》
+
+命令参数：[inject_tool.exe的全路径] start [DLL的端口号]
+
+示例：`C:\inject_tool.exe start 8989 --my_port=9000`
+
+<span style="color: red">注意：</span>`--my_port` 就是你用来接收消息的端口号，这个参数是可选的，不填的话默认就是9000
+
+
+
+## 如何多开？
+
+非常简单！
+不论你是使用《图形界面工具注入》、还是《命令行注入》
+都只需要输入不同的 `[DLL的端口号]` 即可实现多开
+
+- 第1个号：`C:\inject_tool.exe start 8989`
+- 第2个号：`C:\inject_tool.exe start 8990`
+- 第3个号：`C:\inject_tool.exe start 8991`
+- 以此类推...
 
 
 
@@ -49,24 +74,6 @@
 
 - 交流QQ群：716983832
 - 进群解决一切蛇皮问题！
-
-
-
-## help(vworkApi)
-
-**1、监听消息**
-
-![](img3.png)
-
-
-
-**2、发送指令**
-
-![](img1.png)
-
-
-
-![](img2.png)
 
 
 
---
+++ 社区版/Python版演示案例/demo.py
@@ -0,0 +1,90 @@
+import subprocess
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from threading import Thread
+import requests
+import json
+import time
+import winreg
+import sys
+
+
+def get_wx_version():
+    """获取微信版本号"""
+
+    try:
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Tencent\WXWork", 0, winreg.KEY_READ) as key:
+            return winreg.QueryValueEx(key, "Version")[0]
+    except Exception as e:
+        print("打开注册表失败：{}".format(e))
+        return None
+
+
+class HttpServer(BaseHTTPRequestHandler):
+    def do_POST(self):
+        if self.path == "/msg":
+            # 获取数据
+            json_str = self.rfile.read(int(self.headers["content-length"]))
+            json_str = json_str.decode()
+            result = json.loads(json_str)
+            print(result)
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps({"result": "ok"}).encode())
+
+
+if __name__ == '__main__':
+    # 注意！！！请确保你的微信版本是【4.0.0.6024】
+    # 注意！！！请确保你的微信版本是【4.0.0.6024】
+    # 注意！！！请确保你的微信版本是【4.0.0.6024】
+
+    # 校验微信版本号是否正确
+    wx_version = get_wx_version()
+    if wx_version != "4.0.0.6024":
+        print("当前微信版本为：【{}】，请确保你的微信版本是【4.0.0.6024】".format(wx_version))
+        sys.exit(0)
+
+    dll_port = 8748  # dll的端口号
+    my_port = 9001   # 自己的端口号
+
+    # 1、启动http服务【用来监听消息】
+    Thread(target=lambda: HTTPServer(("127.0.0.1", my_port), HttpServer).serve_forever()).start()
+
+    # 2、调用命令行启动微信
+    ret = subprocess.Popen(
+        r'"{}" start {} --my_port={}'.format("../inject_tool.exe", dll_port, my_port),
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    output, error = ret.communicate()
+
+    # 3、循环等待登录
+    while True:
+        time.sleep(1)
+        # 获取登录状态
+        response = requests.post("http://127.0.0.1:{}/api".format(dll_port), json={"type": 1000})
+        ret = json.loads(response.content.decode())
+        is_login = ret["data"]["status"]
+        if is_login:
+            break
+
+    # 4、登录成功，获取个人信息
+    response = requests.post("http://127.0.0.1:{}/api".format(dll_port), json={"type": 1002})
+    ret = json.loads(response.content.decode())
+    wx_info = ret["data"]
+    print(wx_info)
+
+    # 5、发送文本消息
+    requests.post("http://127.0.0.1:{}/api".format(dll_port), json={
+        "type": 3000,
+        "user_id": "FILEASSIST",
+        "msg": "我是文本消息"
+    })
+
+    # ...其他操作
+
+    # 阻塞防止程序退出...
+    while True:
+        time.sleep(1)
